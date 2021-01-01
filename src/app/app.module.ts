5eb1b0f6f03c95590c2384d5eb69ce51de040cfb
Victor K. Maina <vk13runic@gmail.com>
2021-01-01 23:07:31 +0300

    added backgrounds on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProjectComponent } from './project/project.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProjectComponent
+    ProjectComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
